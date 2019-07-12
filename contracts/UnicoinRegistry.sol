905325acfbbbd88481d8d80650cb24b8f2d08401
Luke <lukemeiklejohn@gmail.com>
2019-07-12 11:48:10 +0200

    added new contract functionality

--- contracts/UnicoinRegistry.sol
+++ contracts/UnicoinRegistry.sol
@@ -2,7 +2,7 @@ pragma solidity^0.5.0;
 import "github.com/OpenZeppelin/zeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract UnicoinRegistry {
-    // Creates a struct for users of the plaform. 
+    // Creates a struct for users of the plaform.
     struct User {
         address owned_address;
         string profile_url;
@@ -12,10 +12,12 @@ contract UnicoinRegistry {
     // The mapping below maps all users' addresses to their userID
     mapping (address => uint256) public userAddresses ;
 
+    enum bidStatus {Pending, Accepted, Rejected, Sale, Cancelled}
+
     // Creates a struct for all bids.
     struct Bid {
         uint256 offer;
-        bytes8 status;
+        bidStatus status;
         uint256 publication_Id;
         uint256 owner_Id;
     }
@@ -25,7 +27,6 @@ contract UnicoinRegistry {
     mapping(uint256 => uint256) public bidOwners;
 
     struct Publication {
-        uint256 publication_Id;
         uint256 author_Id;
         string publication_url;
         // The array below will contain all bids received for that work.
@@ -69,9 +70,8 @@ contract UnicoinRegistry {
             require(_sell_price > 0, "Sell price not specified.");
         }
         uint256 _author_Id = userAddresses[msg.sender];
-        uint256 _publication_Id = publications.length + 1;
         uint256[] memory _publication_bids;
-        Publication memory _publication = Publication(_publication_Id, _author_Id, _publication_url, _publication_bids, _isAuction, _isRunning, _sell_price);
+        Publication memory _publication = Publication(_author_Id, _publication_url, _publication_bids, _isAuction, _isRunning, _sell_price);
         uint256 _id = publications.push(_publication);
         publicationOwners[msg.sender].push(_id - 1);
     }
@@ -85,7 +85,7 @@ contract UnicoinRegistry {
         if(publications[_publication_Id].isAuction) {
             require(publications[_publication_Id].isRunning, "Auction is not running.");
             // By default the bid will have a status of Pending until it is accepted or rejected by the author
-            uint256 _id = bids.push(Bid(_offer, "Pending", _publication_Id, userAddresses[msg.sender]));
+            uint256 _id = bids.push(Bid(_offer, bidStatus.Pending, _publication_Id, userAddresses[msg.sender]));
             publications[_publication_Id].publication_bids.push(_id - 1);
         }
         // If the author has specified a flat rate, the buyer doesn't submit a bid but just sends the funds.
@@ -93,10 +93,54 @@ contract UnicoinRegistry {
             // The funds sent should match the sale price specified by the author.
             require(_offer == publications[_publication_Id].sell_price, "Incorrect funds sent.");
             // This 'bid' has a status of sale because the author does not need to evaluate and accept/reject these bids.
-            uint256 _id = bids.push(Bid(_offer, "Sale", _publication_Id, userAddresses[msg.sender]));
+            uint256 _id = bids.push(Bid(_offer, bidStatus.Sale, _publication_Id, userAddresses[msg.sender]));
             publications[_publication_Id].publication_bids.push(_id - 1);
         }
+    }
         
+    // This function allows the auctioneer to accept the bids
+    function acceptBid(uint256 _id) public {
+        uint256 _publication_Id = bids[_id].publication_Id;
+        require(publications[_publication_Id].isAuction, "Publication not an auction.");
+        require(publications[_publication_Id].isRunning, "Auction is not running.");
+        bids[_id].status = bidStatus.Accepted;
+    }
+
+    // This function allows the auctioneer to reject the bids
+    function rejectBid(uint256 _id) public {
+        uint256 _publication_Id = bids[_id].publication_Id;
+        require(publications[_publication_Id].isAuction, "Publication not an auction.");
+        require(publications[_publication_Id].isRunning, "Auction not running.");
+        bids[_id].status = bidStatus.Rejected;
+    }
+
+    // This function allows the auctioneer to cancel the bids
+    function cancelBid(uint256 _id) public {
+        uint256 _publication_Id = bids[_id].publication_Id;
+        require(publications[_publication_Id].isAuction, "Publication not an auction.");
+        require(publications[_publication_Id].isRunning, "Auction not running.");
+        bids[_id].status = bidStatus.Cancelled;
+    }
+    
+    function changeToSale(uint256 _publication_Id, uint256 _sell_price) public {
+        require(publications[_publication_Id].isAuction, "Publication is not an auction");
+        publications[_publication_Id].sell_price = _sell_price;
+        publications[_publication_Id].isAuction = false;
+    }
+   
+    function changeToAuction(uint256 _publication_Id) public {
+        require(!publications[_publication_Id].isAuction, "Publication is already on auction");
+        publications[_publication_Id].sell_price = 0;
+        publications[_publication_Id].isAuction = true;
+    }
+
+    function changeSellPrice(uint256 _publication_Id, uint256 _sell_price) public {
+        require(!publications[_publication_Id].isAuction, "Publication is on auction.");
+        publications[_publication_Id].sell_price = _sell_price;
+    }
+
+    function changeRunningStatus(uint256 _publication_Id) public {
+        publications[_publication_Id].isRunning = !publications[_publication_Id].isRunning;
     }
 
 }
