b54c0031d2c183a33af26f35fe83eed54b9ec948
Christopher Maree <christopher.maree@gmail.com>
2019-07-25 20:58:06 +0200

    Added more functionality for coverage" git push

---
+++ .solcover.js
@@ -0,0 +1,5 @@
+module.exports = {
+    port: 8545,
+    norpc: true,
+    copyPackages: ['openzeppelin-solidity'],
+}
--- truffle-config.js
+++ truffle-config.js
@@ -11,10 +11,8 @@ module.exports = {
     },
     coverage: {
       host: "127.0.0.1", // Localhost (default: none)
-      port: 8555, // Standard Ethereum port (default: none)
+      port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
-      gas: 0xfffffffffff,
-      gasPrice: 0x01
     },
     ropsten: {
       provider: function () {
